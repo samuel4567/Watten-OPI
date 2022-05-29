14bb259014c0731a3d3bb55220db167d9b0d6f38
Samuel Huber csaw5385 <samuel.huber@student.uibk.ac.at>
2022-05-29 20:08:52 +0200

    Add Schlog and Forbe to Card

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,4 +1,28 @@
 package model;
 
 public class Card {
+    private Schlog schlog;
+    private Forbe forbe;
+
+
+
+    public boolean equals(Card o){
+        return this.getSchlog()==o.getSchlog() && this.getForbe()==o.getForbe();
+    }
+
+    public Schlog getSchlog() {
+        return schlog;
+    }
+
+    public void setSchlog(Schlog schlog) {
+        this.schlog = schlog;
+    }
+
+    public Forbe getForbe() {
+        return forbe;
+    }
+
+    public void setForbe(Forbe forbe) {
+        this.forbe = forbe;
+    }
 }
